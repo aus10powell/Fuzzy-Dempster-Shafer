595b496960b7a33999c279c506d5958e33914c4e
AUSTIN POWELL <austinpowell@AUSTINs-MacBook-Pro.local>
2017-01-09 09:30:33 -0800

    changed directory structure

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 
 setup(name = 'fuzzy_dempster_shafer',
-    version = '0.123',
+    version = '0.13',
     description = 'Prediction algorithm based in Dempster-Shafer Fuzzy Logic and Bayesian principles',
     long_description = 'This is an ongoing project to creating a machine learning classification package. \
     The highlight of this package will be the utilization of Dempster-Shafer theory to improve on classifications \
